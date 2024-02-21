5199b684281dd7a6f8c9b4ab7fd5b4dfc0c86537
Jean Wolff <jean.wolff@logicinfo.com>
2024-02-21 08:28:05 -0300

    finishing database chaption

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import { knex } from './database'
 import crypto from 'node:crypto'
+import { env } from './env'
 const app = fastify()
 
 app.get('/hello', async () => {
@@ -19,7 +20,7 @@ app.get('/hello', async () => {
 
 app
     .listen({
-        port: 3333,
+        port: env.PORT,
     })
     .then(() => {
         console.log('HTTP Server Running!')
